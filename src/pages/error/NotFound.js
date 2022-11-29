bcf142394eaf372b4bfec6e1095fc6fc4dd40af6
faraaryaresta <faraaryaresta@gmail.com>
2022-11-29 17:57:31 +0700

    add notfound & server unavailable

--- src/pages/error/NotFound.js
+++ src/pages/error/NotFound.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom';
+import notfound from './../.././assets/img/404.png';
+
 
 const NotFound = () => {
-  return <div>Not Found</div>;
-};
+  return (
+    <div className='container'>     
+      <div className='justify-content-center pt-5'>
+        <img src={notfound}  
+          className="mx-auto d-block pt-5" 
+          alt="notfound"
+          style={{width: 600}}
+        />
+        <h3 className='text-gray-dark text-md text-center pt-2'>Sorry, the page you are looking for doesn’t exist or has been moved.</h3>
+          <div className="d-flex justify-content-center pt-2">
+            <Link
+              to="/admin"
+              className="btn bg-primary text-white text-md me-5 px-4 py-2"
+            >
+              Back to Homepage
+            </Link>
+          </div>
+      </div>  
+      
+    </div>
+  )
+}
 
-export default NotFound;
+export default NotFound
--- src/pages/error/ServerUnavailable.js
+++ src/pages/error/ServerUnavailable.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom';
+import server from './../.././assets/img/505.png';
 
 const ServerUnavailable = () => {
-  return <div>Server Unavailable</div>;
-};
+  return (
+    <div className='container'>     
+      <div className='justify-content-center pt-5'>
+        <img src={server}  
+          className="mx-auto d-block" 
+          alt="notfound"
+          style={{width: 500}}
+        />
+        <div className="d-flex justify-content-center pt-2">
+            <Link
+              to="/admin"
+              className="btn bg-primary text-white text-md me-5 px-5 py-2"
+            >
+              Refresh
+            </Link>
+        </div>
+      </div>  
+    </div>
+  )
+}
 
-export default ServerUnavailable;
+export default ServerUnavailable
