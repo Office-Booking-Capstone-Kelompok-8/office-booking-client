666a36cf372f1d9a5e7ad08898088747c20efe25
mujiburrohman <muji.official06@gmail.com>
2022-12-12 01:27:59 +0700

    update landing

--- src/components/admin/sidebar/Header.js
+++ src/components/admin/sidebar/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './styles.css';
+import './Styles.css';
 
 import Icon from '@mdi/react';
 import { mdiEmailOutline, mdiBellOutline, mdiMagnify } from '@mdi/js';
